7aeabb34510838d99e137012dbf2f4adea7016d1
ionutng <ionutngp@gmail.com>
2023-11-07 14:41:25 +0200

    Create the database using code-first approach

---
+++ PhoneBook/ContactContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PhoneBook;
+
+internal class ContactContext : DbContext
+{
+    public DbSet<Contact> Contacts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("database=phonebook;trusted_connection=true;TrustServerCertificate=True;");
+}
+
+class Contact
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+}
