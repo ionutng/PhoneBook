6a85f947ab2f1f308397cbb8adeeca7318ec42bb
ionutng <ionutngp@gmail.com>
2023-11-07 12:51:17 +0200

    Define the logic of the app

---
+++ PhoneBook/ContactController.cs
@@ -0,0 +1,29 @@
+﻿namespace PhoneBook;
+
+internal class ContactController
+{
+    internal static void AddContact()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void DeleteContact()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void GetContactById()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void GetContacts()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void UpdateContact()
+    {
+        throw new NotImplementedException();
+    }
+}
--- PhoneBook/Program.cs
+++ PhoneBook/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PhoneBook;
+using Spectre.Console;
+
+bool endApp = false;
+
+while (!endApp)
+{
+    var option = AnsiConsole.Prompt(
+        new SelectionPrompt<MenuOptions>()
+        .Title("What would you like to do?")
+        .AddChoices(
+            MenuOptions.AddContact,
+            MenuOptions.UpdateContact,
+            MenuOptions.DeleteContact,
+            MenuOptions.ViewContacts,
+            MenuOptions.ViewContactById,
+            MenuOptions.Quit));
+
+    switch (option)
+    {
+        case MenuOptions.AddContact:
+            ContactController.AddContact();
+            break;
+        case MenuOptions.UpdateContact:
+            ContactController.UpdateContact();
+            break;
+        case MenuOptions.DeleteContact:
+            ContactController.DeleteContact();
+            break;
+        case MenuOptions.ViewContacts:
+            ContactController.GetContacts();
+            break;
+        case MenuOptions.ViewContactById:
+            ContactController.GetContactById();
+            break;
+        case MenuOptions.Quit:
+            Environment.Exit(0);
+            break;
+    }
+}
+
+
+enum MenuOptions {
+    AddContact,
+    UpdateContact,
+    DeleteContact,
+    ViewContacts,
+    ViewContactById,
+    Quit
+}
